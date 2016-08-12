fcc78d15e021a64bad3a3eb711149fa1d8fa29b6
cbilliau <christopher@billiau.com>
2016-08-12 15:52:36 -0400

    Add My Earnings calculations.

--- js/script.js
+++ js/script.js
@@ -3,37 +3,31 @@ angular.module('calcApp', ['ngMessages'])
 
         //  Set $scope var
         $scope.formInvalid = false;
-				$scope.subAmmount = 0;
-				$scope.tipAmmount = 0;
+				$scope.tips = 0;
+				$scope.earningsTipTotal = 0;
+        $scope.earningsMealCount = 0;
+        $scope.avgTipPerMeal = 0;
 
         //  submit function
         $scope.submit = function() {
             if ($scope.mealForm.$valid) {
-								$scope.earningsTipTotal += $scope.tipTotal;
-                console.log('The form is valid');
+                $scope.earningsMealCount++;
+								$scope.earningsTipTotal += tips;
+								$scope.avgTipPerMeal = $scope.earningsTipTotal / $scope.earningsMealCount;
             } else {
-                console.log('The form is invalid');
                 $scope.formInvalid = true;
             }
-
-				$scope.subTotal = function(mealPrice, taxRate){
-					subAmmount = mealPrice + (mealPrice * (taxRate / 100));
-					return subAmmount;
-				};
-
-				$scope.tipTotal = function(sub, tipPerc){
-					tipAmmount = sub * (tipPerc / 100);
-					return tipAmmount;
-				};
-
-				$scope.total = function(sub, tipTotal){
-					// console.log('subtip'+subPlusTip);
-					// console.log('totalTip'+tipAmmount);
-					return subAmmount + tipAmmount;
-				}
         };
-				$scope.earningsTipTotal = 0;
-				$scope.earningsMealCount = 0;
-				$scope.avgTipPerMeal = 0;
+
+				//	calc functions
+        $scope.subTotal = function(mealPrice, taxRate) {
+            return mealPrice + (mealPrice * (taxRate / 100));
+        };
+        $scope.tipTotal = function(sub, tipPerc) {
+						tips = sub * (tipPerc / 100);
+            return tips;
+        };
+
+
 
     }]);
