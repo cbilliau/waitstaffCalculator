b162df62287bbda13c481e34429402c77ade2778
cbilliau <christopher@billiau.com>
2016-08-12 11:37:24 -0400

    Successfully added submit functionality and validation using ng.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 angular.module('calcApp', [ 'ngMessages' ])
 			 .controller('RootCtrl', [ '$scope', function($scope)	{
 
+				//  Set $scope var
+				 $scope.formInvalid = false;
+
+				//  submit function
 				 $scope.submit = function(){
 				 if( $scope.mealForm.$valid ) {
 				     console.log('The form is valid');
 				 } else {
 				     console.log('The form is invalid');
+						 $scope.formInvalid = true;
 				 }
 			 }
 			 }]);
